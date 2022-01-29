d8d1cd40381b81c0aecd9a8015df2ea09b6a0b87
Afreen2907 <73774983+Afreen2907@users.noreply.github.com>
2022-01-29 17:58:01 +0000

    Add files via upload

---
+++ phase1/java-syntax-class-review/src/DoWhile.java
@@ -0,0 +1,14 @@
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int x = 30;
+		do {
+			System.out.println("value of x: "+ x);
+			x++;
+			System.out.println("\n");
+		} while (x < 20);
+
+	}
+
+}
---
+++ phase1/java-syntax-class-review/src/NumericalLiterals.java
@@ -0,0 +1,13 @@
+
+public class NumericalLiterals {
+
+	public static void main(String[] args) {
+		long creditCardNumber = 1234_5678_9012_3456L; //useful for readability
+		long socialSecurityNumber = 999_99_9999L;
+		long hexBytes = 0XFF_EC_DE_5E;
+		
+		System.out.println(hexBytes);
+
+	}
+
+}
